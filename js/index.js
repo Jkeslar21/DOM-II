bbbf4e8065b0d8dd0d44d9410303d8d8801c5a1a
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-01-29 14:55:54 -0700

    8 of 10 mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // Your code goes here
+const logoHeader = document.querySelector('.logo-heading');
+const bodyAll = document.querySelector('body');
+const btn1 = document.querySelector('#one');
+const btn2 = document.querySelector('#two');
+const btn3 = document.querySelector('#three');
+const html = document.querySelector('html');
+
+logoHeader.addEventListener('mouseover', e => {
+    logoHeader.style.color = '#17A2B8';
+  });
+  logoHeader.addEventListener('mouseout', e => {
+      logoHeader.style.color = 'black';
+  });
+bodyAll.addEventListener('keydown', e => {
+    bodyAll.style.color = 'purple';
+    bodyAll.style.backgroundColor = 'lightgrey';
+    logoHeader.style.color = 'lightblue';
+});
+bodyAll.addEventListener('keyup', e => {
+    bodyAll.style.color = 'black';
+    bodyAll.style.backgroundColor = 'white';
+    logoHeader.style.color = 'black';
+});
+btn1.addEventListener('click', e => {
+    alert('You successfully signed up for Fun In The Sun!');
+});
+btn2.addEventListener('dblclick', e => {
+    alert('You double clicked this button and signed up for Mountain Excursion twice!');
+});
+btn3.addEventListener('dragstart', e => {
+    alert('Why are you dragging me?  I was comfortable where I was!');
+});
+html.addEventListener('copy', e => {
+    alert("Copycat!");
+});
